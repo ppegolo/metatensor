cbe4a60713e3ef97c7d23ee276743e9056767838
ppegolo <paolo.pegolo@epfl.ch>
2024-10-24 17:52:31 +0200

    Add numpy tests for EquivariantPCA

---
+++ python/metatensor-learn/tests/_pca.py
@@ -0,0 +1,51 @@
+import os
+
+import pytest
+
+import metatensor
+
+
+torch = pytest.importorskip("torch")
+
+from metatensor.learn.decomposition import EquivariantPCA  # noqa: E402
+
+from ._rotation_utils import WignerDReal  # noqa: E402
+
+
+DATA_ROOT = os.path.join(
+    os.path.dirname(__file__), "..", "..", "metatensor-operations", "tests", "data"
+)
+
+
+@pytest.fixture
+def tensor():
+    tensor = metatensor.load(os.path.join(DATA_ROOT, "qm7-spherical-expansion.npz"))
+    tensor = tensor.to(arrays="torch")
+    tensor = metatensor.remove_gradients(tensor)
+    return tensor
+
+
+@pytest.fixture
+def wigner_d_real():
+    return WignerDReal(lmax=4, angles=(0.87641, 1.8729, 0.9187))
+
+
+def test_equivariance(tensor, wigner_d_real):
+    """
+    Tests that application of the PCA is equivariant to O3 transformation of the input.
+    """
+    # Define input and rotated input
+    x = tensor
+    Rx = wigner_d_real.transform_tensormap_o3(x)
+
+    # Define the PCA calculator
+    epca = EquivariantPCA(
+        n_components=10,
+        key_lambda_name="o3_lambda",
+    )
+
+    #
+    Rfx = wigner_d_real.transform_tensormap_o3(epca.fit_transform(x))  # R . epca(x)
+    fRx = epca.fit_transform(Rx)  # epca(R . x)
+
+    assert metatensor.allclose(fRx, Rfx, atol=1e-10, rtol=1e-10)
